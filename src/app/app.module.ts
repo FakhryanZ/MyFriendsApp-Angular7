73dbf4902b9daac03357d4913cd49f2d17d559b5
FakhryanZ <fakhryanmuhammad@gmail.com>
2019-05-20 22:06:42 +0700

    Add Component Friends

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainNavbarComponent } from './main-navbar/main-navbar.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MainNavbarComponent,
-    ContactMeComponent
+    ContactMeComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path:'',component:HomeComponent},
-      {path:'contactus',component:ContactMeComponent}
+      {path:'contactus',component:ContactMeComponent},
+      {path:'friend',component:FriendsComponent}
     ]),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
