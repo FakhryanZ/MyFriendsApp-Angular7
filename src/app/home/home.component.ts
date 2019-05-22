5976ffeb156dfe43f5176d14f1e58d05bb5c89dd
FakhryanZ <fakhryanmuhammad@gmail.com>
2019-05-22 08:39:40 +0700

    fixing Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "./../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements OnInit {
-  title:string = 'My Friends App';
-  constructor() { }
+export class HomeComponent {
+  title: string = "My Friends App";
+  form = new FormGroup({
+    username: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+  });
+  invalidLogin: boolean;
 
-  ngOnInit() {
+  get username() {
+    return this.form.get("username");
   }
 
+  get password() {
+    return this.form.get("password");
+  }
+  constructor(
+    private router : Router,
+    private authService : AuthService
+  ) {}
+
+  signIn(credentials){
+    this.authService.login(credentials)
+      .subscribe(result =>{
+        if (result)
+          this.router.navigate(['friend']);
+        else
+          this.invalidLogin = true;
+      });
+  }
 }
