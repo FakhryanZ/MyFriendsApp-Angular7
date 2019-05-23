d42806763b9b3cfd99dfc0956016b9cffe0cddb6
FakhryanZ <fakhryanmuhammad@gmail.com>
2019-05-23 15:28:20 +0700

    Fixing Contact-Me

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -7,27 +7,36 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
   styleUrls: ['./contact-me.component.css']
 })
 export class ContactMeComponent implements OnInit {
-  form = new FormGroup({
-    name : new FormControl('', Validators.required),
-    email : new FormControl('',[Validators.required,Validators.email]),
-    message : new FormControl('',Validators.required)
-  })
-
-  get name(){
-    return this.form.get('name');
-  }
-
-  get email(){
-    return this.form.get('email');
-  }
-
-  get message(){
-    return this.form.get('message');
-  }
-
+  ContactForm
+  ContactData: any= []
   constructor() { }
 
   ngOnInit() {
+    this.GetData()
+    this.ContactForm = new FormGroup({
+    name : new FormControl('', Validators.required),
+    email : new FormControl('',[Validators.required,Validators.email]),
+    message : new FormControl('',Validators.required)
+    })
+  }
+
+  AddContact(name, email, message){
+    let contact = {
+      name: name,
+      email: email,
+      message : message
+    }
+    sessionStorage.setItem(name, JSON.stringify(contact))
+    this.GetData()
+  }
+
+  GetData(){
+    for (let i = 0; i < sessionStorage.length; i++) {
+      let key = sessionStorage.key(i);
+      let value = sessionStorage.getItem(key);
+      this.ContactData[i] = JSON.parse(value)
+      console.log(this.ContactData);
+    }
   }
 
 }
